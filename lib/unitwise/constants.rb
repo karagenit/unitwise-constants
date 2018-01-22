5c02c0d0b448ee0426d19ae3039e350d44c3114a
Caleb Smith <karagenit@outlook.com>
2018-01-22 10:46:52 -0500

    Add Constants

--- lib/unitwise/constants.rb
+++ lib/unitwise/constants.rb
@@ -1,5 +1,9 @@
 require 'unitwise'
 
 module Unitwise
-  GRAVITY = Unitwise(9.81, 'm/s2')
+  GRAVITY     = Unitwise(9.81, 'm/s2')
+  LIGHTSPEED  = Unitwise(3.00E8, 'm/s')
+  GRAVITATION = Unitwise(6.67E-11, 'N.m2/kg2')
+  AVOGADRO    = Unitwise(6.02E23, '1/mol')
+  CHARGE      = Unitwise(1.60E-19, 'C')
 end
